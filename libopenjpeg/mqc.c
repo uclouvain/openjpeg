9fa68ca8111c83ebac19718530328bd5c0700820
Antonin Descampe <antonin@gmail.com>
2004-07-21 05:18:57 +0000

    Bug fixed in MQ decoder initialization (keyword comment : initbug1)

--- libopenjpeg/mqc.c
+++ libopenjpeg/mqc.c
@@ -536,7 +536,10 @@ void mqc_init_dec(unsigned char *bp, int len)
   mqc_start = bp;
   mqc_end = bp + len;
   mqc_bp = bp;
-  mqc_c = *mqc_bp << 16;
+  //add antonin initbug1
+  if (len==0) mqc_c = 0xff << 16;
+  else mqc_c = *mqc_bp << 16;
+  //dda
   mqc_bytein();
   mqc_c <<= 7;
   mqc_ct -= 7;
