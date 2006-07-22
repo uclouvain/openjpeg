4b3aabebd29a9d0b2b9fea0c76a18a4317c8d4de
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-07-22 03:35:17 +0000

    ENH: Fix warning about assignment discards qualifiers from pointer target type

--- codec/compat/getopt.c
+++ codec/compat/getopt.c
@@ -45,7 +45,7 @@ int opterr = 1,			/* if error message should be printed */
  optind = 1,			/* index into parent argv vector */
  optopt,			/* character checked for validity */
  optreset;			/* reset getopt */
-char *optarg;			/* argument associated with option */
+const char *optarg;			/* argument associated with option */
 
 #define	BADCH	(int)'?'
 #define	BADARG	(int)':'
@@ -57,7 +57,7 @@ char *optarg;			/* argument associated with option */
  */
 int getopt(int nargc, char *const *nargv, const char *ostr) {
 #  define __progname nargv[0]
-  static char *place = EMSG;	/* option letter processing */
+  static const char *place = EMSG;	/* option letter processing */
   char *oli;			/* option letter list index */
 
   if (optreset || !*place) {	/* update scanning pointer */
