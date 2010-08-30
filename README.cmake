e4c4d146e17853933d90a89f04ce2ad79efddcb7
Antonin Descampe <antonin@gmail.com>
2010-08-30 10:14:34 +0000

    Update the README.cmake file : an instruction was missing to run the tests correctly.

--- README.cmake
+++ README.cmake
@@ -4,15 +4,17 @@ Basic instructions on how to build using CMake (CMake 2.4.5 or newer is required
   cd openjpeg-read-only
   cmake -G "Unix Makefiles"
   make
-  ./bin/j2k_to_image
+	
+Executables are located in the ./bin directory.
 	
 For MacOSX users, if instructions above fail, try to add "-DCMAKE_OSX_ARCHITECTURES:STRING=i386" to the cmake command above.
 	
 To enable testing (and automatic result upload to http://my.cdash.org/index.php?project=OPENJPEG) : 
 
 	svn co http://openjpeg.googlecode.com/svn/trunk openjpeg-read-only
-  cd openjpeg-read-only
-  cmake -G "Unix Makefiles" -DBUILD_TESTING:BOOL=ON -DJPEG2000_CONFORMANCE_DATA_ROOT:PATH=/path/to/your/JPEG2000/test/files
-  make Experimental
+	cd openjpeg-read-only
+	cmake -G "Unix Makefiles" -DBUILD_TESTING:BOOL=ON -DJPEG2000_CONFORMANCE_DATA_ROOT:PATH=/path/to/your/JPEG2000/test/files
+	make
+	make Experimental
 
 Note : JPEG2000 test files are available here : http://www.crc.ricoh.com/~gormish/jpeg2000conformance/
