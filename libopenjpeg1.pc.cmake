d2d6e1fd6ff9a09631cb7c7353cbdfb74f1ccc4e
Rex Dieter <rdieter@gmail.com>
2011-01-13 19:29:18 +0000

    add pkg-config support to cmake

---
+++ libopenjpeg1.pc.cmake
@@ -0,0 +1,12 @@
+prefix=${CMAKE_INSTALL_PREFIX}
+bindir=${OPENJPEG_INSTALL_BIN_DIR}
+datadir=${OPENJPEG_INSTALL_DATA_DIR}
+libdir=${OPENJPEG_INSTALL_LIB_DIR}
+includedir=${OPENJPEG_INSTALL_INCLUDE_DIR}
+
+Name: openjpeg
+Description: JPEG2000 files library
+URL: http://www.openjpeg.org/ 
+Version: ${OPENJPEG_VERSION} 
+Libs: -L${libdir} -lopenjpeg
+Cflags: -I${includedir}
