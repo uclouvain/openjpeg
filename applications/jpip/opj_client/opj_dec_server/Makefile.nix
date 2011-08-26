3b9d4c8d505c4612f6d6945c9cfbe64bac37d814
Kaori Hagihara <khagihara@users.noreply.github.com>
2011-08-26 11:50:50 +0000

    fixed Makefile.nix to load openjpeg library from this archive

--- applications/jpip/opj_client/opj_dec_server/Makefile.nix
+++ applications/jpip/opj_client/opj_dec_server/Makefile.nix
@@ -1,7 +1,10 @@
-LIBDIR = ../../libopenjpip
-LIBFNAME = $(LIBDIR)/libopenjpip_local.a
-CFLAGS  = -O3 -Wall -I$(LIBDIR)
-LDFLAGS = -L$(LIBDIR) -lm -lopenjpeg -lopenjpip_local
+J2KINCDIR = ../../../../libopenjpeg
+J2KCNFDIR = ../../../..
+J2KLIBDIR = $(J2KINCDIR)/.libs
+JPIPLIBDIR = ../../libopenjpip
+LIBFNAME = $(JPIPLIBDIR)/libopenjpip_local.a $(J2KLIBDIR)/libopenjpeg.a
+CFLAGS  = -O3 -Wall -I$(JPIPLIBDIR) -I$(J2KINCDIR) -I$(J2KCNFDIR)
+LDFLAGS = -L$(JPIPLIBDIR) -L$(J2KLIBDIR) -lm -lopenjpip_local
 #-lws2_32
 
 ALL = opj_dec_server
@@ -9,6 +12,6 @@ ALL = opj_dec_server
 all: $(ALL)
 
 opj_dec_server: opj_dec_server.o jp2k_decoder.o imgsock_manager.o jpipstream_manager.o cache_manager.o $(LIBFNAME)
-	$(CC) $(CFLAGS) $< jp2k_decoder.o imgsock_manager.o jpipstream_manager.o cache_manager.o $(LDFLAGS) -o $@
+	$(CC) $(CFLAGS) $< jp2k_decoder.o imgsock_manager.o jpipstream_manager.o cache_manager.o $(LDFLAGS) $(LIBFNAME) -o $@
 clean:
 	rm -f $(ALL) *.o *~
