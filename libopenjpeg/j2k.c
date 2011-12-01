5c363b76eeeea843967103f839d2ef68c8cc2499
Mickael Savinaud <savmickael@users.noreply.github.com>
2011-12-01 13:46:09 +0000

    [trunk] fix a bug during the creation of the codestream index

--- libopenjpeg/j2k.c
+++ libopenjpeg/j2k.c
@@ -8114,7 +8114,7 @@ opj_bool j2k_allocate_tile_element_cstr_index(opj_j2k_v2_t *p_j2k)
 {
 	OPJ_UINT32 it_tile=0;
 
-	p_j2k->cstr_index->nb_of_tiles = p_j2k->m_cp.tw * p_j2k->m_cp.tw;
+	p_j2k->cstr_index->nb_of_tiles = p_j2k->m_cp.tw * p_j2k->m_cp.th;
 	p_j2k->cstr_index->tile_index = (opj_tile_index_t*)opj_calloc(p_j2k->cstr_index->nb_of_tiles, sizeof(opj_tile_index_t));
 	if (!p_j2k->cstr_index->tile_index)
 		return OPJ_FALSE;
