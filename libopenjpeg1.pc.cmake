545680cdb4d24fc5dfdfc65736696c2359c70545
Rex Dieter <rdieter@gmail.com>
2012-02-28 17:52:33 +0000

    allow finer-grain control of header location via OPENJPEG_INSTALL_INCLUDE_DIR

--- libopenjpeg1.pc.cmake
+++ libopenjpeg1.pc.cmake
@@ -2,7 +2,7 @@ prefix=@CMAKE_INSTALL_PREFIX@
 bindir=${prefix}/@OPENJPEG_INSTALL_BIN_DIR@
 datadir=${prefix}/@OPENJPEG_INSTALL_DATA_DIR@
 libdir=${prefix}/@OPENJPEG_INSTALL_LIB_DIR@
-includedir=${prefix}/@OPENJPEG_INSTALL_INCLUDE_DIR@/@OPENJPEG_INSTALL_SUBDIR@
+includedir=${prefix}/@OPENJPEG_INSTALL_INCLUDE_DIR@
 
 Name: openjpeg
 Description: JPEG2000 files library
