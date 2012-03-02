c433c2f2e230fdbb41e66ab2080d7beb72a00efa
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2012-03-02 09:26:15 +0000

    [trunk] Backport toplevel files from 1.5

--- libopenjpeg1.pc.cmake
+++ libopenjpeg1.pc.cmake
@@ -1,8 +1,8 @@
 prefix=@CMAKE_INSTALL_PREFIX@
-bindir=@OPENJPEG_INSTALL_BIN_DIR@
-datadir=@OPENJPEG_INSTALL_DATA_DIR@
-libdir=@OPENJPEG_INSTALL_LIB_DIR@
-includedir=@OPENJPEG_INSTALL_INCLUDE_DIR@
+bindir=${prefix}/@OPENJPEG_INSTALL_BIN_DIR@
+datadir=${prefix}/@OPENJPEG_INSTALL_DATA_DIR@
+libdir=${prefix}/@OPENJPEG_INSTALL_LIB_DIR@
+includedir=${prefix}/@OPENJPEG_INSTALL_INCLUDE_DIR@
 
 Name: openjpeg
 Description: JPEG2000 files library
