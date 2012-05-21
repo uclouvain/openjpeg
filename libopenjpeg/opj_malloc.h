8cd13f3a55196ac1c727f65adf78900b41c8750b
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2012-05-21 10:34:35 +0000

    [1.5] Build fails on AIX
    Fixes issue 139

--- libopenjpeg/opj_malloc.h
+++ libopenjpeg/opj_malloc.h
@@ -84,7 +84,7 @@ Allocate memory aligned to a 16 byte boundry
 	#if defined(__sun)
 		#define HAVE_MEMALIGN
 	/* Linux x86_64 and OSX always align allocations to 16 bytes */
-	#elif !defined(__amd64__) && !defined(__APPLE__)	
+	#elif !defined(__amd64__) && !defined(__APPLE__) && !defined(_AIX)
 		#define HAVE_MEMALIGN
 		#include <malloc.h>			
 	#endif
