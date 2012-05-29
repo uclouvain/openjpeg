6da56bb11afa8fa35d71e769c6adff41a944b9a0
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2012-05-29 10:19:45 +0000

    [1.5] Fix compilation on FreeBSD. Thanks to rafael carre (funman@videolan.org) for patch.
    Fixes issue 111

--- libopenjpeg/opj_malloc.h
+++ libopenjpeg/opj_malloc.h
@@ -83,6 +83,8 @@ Allocate memory aligned to a 16 byte boundry
 #else /* Not _WIN32 */
 	#if defined(__sun)
 		#define HAVE_MEMALIGN
+  #elif defined(__FreeBSD__)
+    #define HAVE_POSIX_MEMALIGN
 	/* Linux x86_64 and OSX always align allocations to 16 bytes */
 	#elif !defined(__amd64__) && !defined(__APPLE__) && !defined(_AIX)
 		#define HAVE_MEMALIGN
