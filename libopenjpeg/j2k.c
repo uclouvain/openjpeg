679f91482e912fc2f04d8960b789a4c39b44e4c2
Mickael Savinaud <savmickael@users.noreply.github.com>
2012-08-09 16:48:48 +0000

    rename j2k_read_qcd_v2 to opj_j2k_read_qcd

--- libopenjpeg/j2k.c
+++ libopenjpeg/j2k.c
@@ -602,12 +602,10 @@ static opj_bool j2k_write_qcd_v2(	opj_j2k_v2_t *p_j2k,
  * @param	p_header_size	the size of the data contained in the QCD marker.
  * @param	p_manager		the user event manager.
 */
-static opj_bool j2k_read_qcd_v2 (
-					opj_j2k_v2_t *p_j2k,
-					OPJ_BYTE * p_header_data,
-					OPJ_UINT32 p_header_size,
-					struct opj_event_mgr * p_manager
-					);
+static opj_bool opj_j2k_read_qcd (  opj_j2k_v2_t *p_j2k,
+                                    OPJ_BYTE * p_header_data,
+                                    OPJ_UINT32 p_header_size,
+                                    opj_event_mgr_t * p_manager );
 
 /**
 Write the QCC marker (quantization component)
@@ -1422,7 +1420,7 @@ const opj_dec_memory_marker_handler_t j2k_memory_marker_handler_tab [] =
   {J2K_MS_COD, J2K_STATE_MH | J2K_STATE_TPH, opj_j2k_read_cod},
   {J2K_MS_COC, J2K_STATE_MH | J2K_STATE_TPH, opj_j2k_read_coc},
   {J2K_MS_RGN, J2K_STATE_MH | J2K_STATE_TPH, opj_j2k_read_rgn},
-  {J2K_MS_QCD, J2K_STATE_MH | J2K_STATE_TPH, j2k_read_qcd_v2},
+  {J2K_MS_QCD, J2K_STATE_MH | J2K_STATE_TPH, opj_j2k_read_qcd},
   {J2K_MS_QCC, J2K_STATE_MH | J2K_STATE_TPH, j2k_read_qcc_v2},
   {J2K_MS_POC, J2K_STATE_MH | J2K_STATE_TPH, j2k_read_poc_v2},
   {J2K_MS_SIZ, J2K_STATE_MHSIZ , opj_j2k_read_siz},
@@ -3336,12 +3334,11 @@ opj_bool j2k_write_qcd_v2(	opj_j2k_v2_t *p_j2k,
  * @param	p_header_size	the size of the data contained in the QCD marker.
  * @param	p_manager		the user event manager.
 */
-opj_bool j2k_read_qcd_v2 (
-				    opj_j2k_v2_t *p_j2k,
-					OPJ_BYTE * p_header_data,
-					OPJ_UINT32 p_header_size,
-					struct opj_event_mgr * p_manager
-					)
+static opj_bool opj_j2k_read_qcd (  opj_j2k_v2_t *p_j2k,
+                                    OPJ_BYTE * p_header_data,
+                                    OPJ_UINT32 p_header_size,
+                                    opj_event_mgr_t * p_manager 
+                                    )
 {
 	/* preconditions */
 	assert(p_header_data != 00);
