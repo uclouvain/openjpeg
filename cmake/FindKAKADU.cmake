caf4e46e069fea1bfd274cb579dc5b9c31bf5ba0
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2014-02-07 10:37:06 +0000

    Update kakadu cmake module

--- cmake/FindKAKADU.cmake
+++ cmake/FindKAKADU.cmake
@@ -1,9 +1,9 @@
 #
-# this module looks for KAKADu
+# this module looks for KAKADU
 # http://www.kakadusoftware.com/
 #
 #
-#  Copyright (c) 2006-2011 Mathieu Malaterre <mathieu.malaterre@gmail.com>
+#  Copyright (c) 2006-2014 Mathieu Malaterre <mathieu.malaterre@gmail.com>
 #
 #  Redistribution and use is allowed according to the terms of the New
 #  BSD license.
@@ -14,6 +14,11 @@ find_program(KDU_EXPAND_EXECUTABLE
   kdu_expand
   )
 
+find_program(KDU_COMPRESS_EXECUTABLE
+  kdu_compress
+  )
+
 mark_as_advanced(
   KDU_EXPAND_EXECUTABLE
+  KDU_COMPRESS_EXECUTABLE
   )
