9863d8d1d24f2331193d5a42473fbd2422debee8
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2014-03-03 08:49:28 +0000

    [trunk] Avoid conflicting declarion for fseek in openjp2 vs fseek in openjpip

--- src/lib/openjp2/opj_includes.h
+++ src/lib/openjp2/opj_includes.h
@@ -56,9 +56,11 @@
   ftello() only on systems with special LFS support since some systems
   (e.g. FreeBSD) support a 64-bit off_t by default.
 */
+#if !defined(OPENJPIP_H_) && defined(SERVER) /* server implementation uses fcgi/fseek*/
 #if defined(OPJ_HAVE_FSEEKO)
 #  define fseek  fseeko
 #  define ftell  ftello
+#endif
 #endif
 
 
