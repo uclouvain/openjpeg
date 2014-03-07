c302979738c3a50b102f76f38109cd0ec5149691
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2014-03-07 11:08:35 +0000

    [trunk] Remove simple warning about functions not used

--- src/bin/mj2/opj_mj2_compress.c
+++ src/bin/mj2/opj_mj2_compress.c
@@ -61,13 +61,6 @@ static void warning_callback(const char *msg, void *client_data) {
 	FILE *stream = (FILE*)client_data;
 	fprintf(stream, "[WARNING] %s", msg);
 }
-/**
-sample debug callback expecting a FILE* client object
-*/
-static void info_callback(const char *msg, void *client_data) {
-	FILE *stream = (FILE*)client_data;
-	fprintf(stream, "[INFO] %s", msg);
-}
 
 /* -------------------------------------------------------------------------- */
 
