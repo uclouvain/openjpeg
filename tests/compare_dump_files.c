f7bc0b49cbdd68e2c16f623d7c6617ded6ace0bf
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2014-03-10 10:12:25 +0000

    [trunk] Fix typo in the code

--- tests/compare_dump_files.c
+++ tests/compare_dump_files.c
@@ -174,7 +174,7 @@ int main(int argc, char **argv)
 cleanup:
   /*Close File*/
   if(fbase) fclose(fbase);
-  if(ftell) fclose(ftest);
+  if(ftest) fclose(ftest);
 
   /* Free memory*/
   free(inParam.base_filename);
