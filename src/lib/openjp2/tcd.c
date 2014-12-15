9a21e1eef06672b8a546430e9cb578c56d7858cf
Matthieu Darbois <mayeut@users.noreply.github.com>
2014-12-15 18:48:50 +0000

    [trunk] removed warnings when building tcd.c (Update issue 442)

--- src/lib/openjp2/tcd.c
+++ src/lib/openjp2/tcd.c
@@ -1079,7 +1079,7 @@ static OPJ_BOOL opj_tcd_code_block_enc_allocate_data (opj_tcd_cblk_enc_t * p_cod
 {
 	OPJ_UINT32 l_data_size;
 	
-	l_data_size = (p_code_block->x1 - p_code_block->x0) * (p_code_block->y1 - p_code_block->y0) * sizeof(OPJ_UINT32);
+	l_data_size = (OPJ_UINT32)((p_code_block->x1 - p_code_block->x0) * (p_code_block->y1 - p_code_block->y0) * (OPJ_INT32)sizeof(OPJ_UINT32));
 	
 	if (l_data_size > p_code_block->data_size) {
 		if (p_code_block->data) {
