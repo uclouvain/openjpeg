2452bb7c45256c82b895ab25875aa3d77aedf90d
mayeut <mayeut@users.noreply.github.com>
2015-07-26 19:01:52 +0200

    Remove some warnings when building
    Update #442

--- src/bin/jp2/convert.h
+++ src/bin/jp2/convert.h
@@ -72,14 +72,14 @@ void scale_component(opj_image_comp_t* component, OPJ_UINT32 precision);
 
 /* planar / interleaved conversions */
 typedef void (* convert_32s_CXPX)(const OPJ_INT32* pSrc, OPJ_INT32* const* pDst, OPJ_SIZE_T length);
-const convert_32s_CXPX convert_32s_CXPX_LUT[5];
+extern const convert_32s_CXPX convert_32s_CXPX_LUT[5];
 typedef void (* convert_32s_PXCX)(OPJ_INT32 const* const* pSrc, OPJ_INT32* pDst, OPJ_SIZE_T length, OPJ_INT32 adjust);
-const convert_32s_PXCX convert_32s_PXCX_LUT[5];
+extern const convert_32s_PXCX convert_32s_PXCX_LUT[5];
 /* bit depth conversions */
 typedef void (* convert_XXx32s_C1R)(const OPJ_BYTE* pSrc, OPJ_INT32* pDst, OPJ_SIZE_T length);
-const convert_XXx32s_C1R convert_XXu32s_C1R_LUT[9]; /* up to 8bpp */
+extern const convert_XXx32s_C1R convert_XXu32s_C1R_LUT[9]; /* up to 8bpp */
 typedef void (* convert_32sXXx_C1R)(const OPJ_INT32* pSrc, OPJ_BYTE* pDst, OPJ_SIZE_T length);
-const convert_32sXXx_C1R convert_32sXXu_C1R_LUT[9]; /* up to 8bpp */
+extern const convert_32sXXx_C1R convert_32sXXu_C1R_LUT[9]; /* up to 8bpp */
 
 
 /* TGA conversion */
