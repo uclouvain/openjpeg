20e80689a507be7c053d1e782f38c5d1adfd7f0d
slmistry <shailesh.mistry@hotmail.co.uk>
2015-09-10 00:11:14 +0100

    Resolve warning about __STDC_VERSION__

--- src/lib/openjp2/opj_includes.h
+++ src/lib/openjp2/opj_includes.h
@@ -109,7 +109,7 @@
 
 
 /* Are restricted pointers available? (C99) */
-#if (__STDC_VERSION__ != 199901L)
+#if !defined(__STDC_VERSION__) || (__STDC_VERSION__ != 199901L)
 	/* Not a C99 compiler */
 	#ifdef __GNUC__
 		#define restrict __restrict__
