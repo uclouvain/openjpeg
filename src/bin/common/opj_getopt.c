b2fcd688575a36b8c5ce194e896e7f93a9ce4bd2
Stefan Weil <sw@weilnetz.de>
2015-10-07 11:58:11 +0200

    Fix warnings for C++
    g++ complains about invalid conversions like these ones:
    
    error: invalid conversion from ‘const char*’ to ‘char*’ [-fpermissive]
    error: invalid conversion from ‘void*’ to ‘opj_precision* {aka opj_prec*}’ [-fpermissive]
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- src/bin/common/opj_getopt.c
+++ src/bin/common/opj_getopt.c
@@ -66,7 +66,7 @@ void opj_reset_options_reading(void) {
 int opj_getopt(int nargc, char *const *nargv, const char *ostr) {
 #  define __progname nargv[0]
   static char *place = EMSG;	/* option letter processing */
-  char *oli = NULL;			/* option letter list index */
+  const char *oli = NULL;	/* option letter list index */
 
   if (opj_optreset || !*place) {	/* update scanning pointer */
     opj_optreset = 0;
@@ -125,7 +125,7 @@ int opj_getopt(int nargc, char *const *nargv, const char *ostr) {
 int opj_getopt_long(int argc, char * const argv[], const char *optstring,
 const opj_option_t *longopts, int totlen) {
 	static int lastidx,lastofs;
-	char *tmp;
+	const char *tmp;
 	int i,len;
 	char param = 1;
 
