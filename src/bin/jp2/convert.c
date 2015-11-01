ce26e523dd2345413ee7990afdf84f2c4f13bc88
mayeut <mayeut@users.noreply.github.com>
2015-11-01 16:56:50 +0100

    Fix compiler warnings

--- src/bin/jp2/convert.c
+++ src/bin/jp2/convert.c
@@ -558,8 +558,8 @@ struct tga_header
 };
 #endif /* INFORMATION_ONLY */
 
-static unsigned short get_ushort(unsigned char *data) {
-    unsigned short val = *(unsigned short *)data;
+static unsigned short get_ushort(const unsigned char *data) {
+    unsigned short val = *(const unsigned short *)data;
 #ifdef OPJ_BIG_ENDIAN
     val = ((val & 0xffU) << 8) | (val >> 8);
 #endif
