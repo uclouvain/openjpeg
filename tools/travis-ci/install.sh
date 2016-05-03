90ecbc54dec871d4ae4f6c1209fca01c5626c439
Julien Malik <julien.malik@paraiso.me>
2016-05-03 13:58:03 +0200

    Use cmake 3.5 on linux builds inside Travis

--- tools/travis-ci/install.sh
+++ tools/travis-ci/install.sh
@@ -35,7 +35,7 @@ if [ "${COVERITY_SCAN_BRANCH:-}" == "1" ] || [ "${OPJ_CI_ABI_CHECK:-}" == "1" ];
 	exit 0
 fi
 
-if [ "${OPJ_CI_ASAN:-}" == "1" ]; then
+if [ "${TRAVIS_OS_NAME:-}" == "linux" ]; then
 	# We need a new version of cmake than travis-ci provides
 	wget --no-check-certificate -qO - https://cmake.org/files/v3.5/cmake-3.5.2-Linux-x86_64.tar.gz | tar -xz
 	# copy to a directory that will not changed every version
