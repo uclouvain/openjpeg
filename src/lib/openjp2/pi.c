bf845a3892a6701a3766ea6478abc9cf560362ef
Matthieu Darbois <mayeut@users.noreply.github.com>
2016-09-08 07:34:46 +0200

    Cast to size_t before multiplication
    Need to cast to size_t before multiplication otherwise overflow check is useless.

--- src/lib/openjp2/pi.c
+++ src/lib/openjp2/pi.c
@@ -1241,7 +1241,7 @@ opj_pi_iterator_t *opj_pi_create_decode(opj_image_t *p_image,
 	l_current_pi->include = 00;
 	if (l_step_l <= (SIZE_MAX / (l_tcp->numlayers + 1U)))
 	{
-		l_current_pi->include = (OPJ_INT16*) opj_calloc((l_tcp->numlayers +1) * l_step_l, sizeof(OPJ_INT16));
+		l_current_pi->include = (OPJ_INT16*) opj_calloc((size_t)(l_tcp->numlayers + 1U) * l_step_l, sizeof(OPJ_INT16));
 	}
 
 	if
