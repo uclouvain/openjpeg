fd205f457b157e925e2a6eb03aba397b45b0ed4e
Even Rouault <even.rouault@spatialys.com>
2018-06-16 16:17:58 +0200

    opj_compress: try to make help message of -c switch clearer (fixes #1117)

--- src/bin/jp2/opj_compress.c
+++ src/bin/jp2/opj_compress.c
@@ -203,7 +203,7 @@ static void encode_help_display(void)
     fprintf(stdout,
             "    to the highest resolution level and subsequent records to lower \n");
     fprintf(stdout,
-            "    resolution levels. The last specified record is right-shifted for each \n");
+            "    resolution levels. The last specified record is halved successively for each \n");
     fprintf(stdout, "    remaining lower resolution levels.\n");
     fprintf(stdout, "    Default: 2^15x2^15 at each resolution.\n");
     fprintf(stdout, "-t <tile width>,<tile height>\n");
