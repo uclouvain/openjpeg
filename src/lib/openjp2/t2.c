b2df8a57bbfeecde02d0cfc7bdfb4f1b3493b90b
Jarrel Seah <jarrelscy@gmail.com>
2019-11-18 08:57:32 +1100

    Update t2.c

--- src/lib/openjp2/t2.c
+++ src/lib/openjp2/t2.c
@@ -1313,6 +1313,7 @@ static OPJ_BOOL opj_t2_read_packet_data(opj_t2_t* p_t2,
 
     OPJ_ARG_NOT_USED(p_t2);
     OPJ_ARG_NOT_USED(pack_info);
+    int truncate = 0;
 
     l_band = l_res->bands;
     for (bandno = 0; bandno < l_res->numbands; ++bandno) {
@@ -1346,7 +1347,7 @@ static OPJ_BOOL opj_t2_read_packet_data(opj_t2_t* p_t2,
                     ++l_cblk->numsegs;
                 }
             }
-            int truncate = 0;
+            truncate = 0;
             do {
                 /* Check possible overflow (on l_current_data only, assumes input args already checked) then size */
                if ((((OPJ_SIZE_T)l_current_data + (OPJ_SIZE_T)l_seg->newlen) <
@@ -1358,7 +1359,7 @@ static OPJ_BOOL opj_t2_read_packet_data(opj_t2_t* p_t2,
                                  l_seg->newlen, l_current_data, p_src_data, p_max_length, cblkno, p_pi->precno, bandno, p_pi->resno,
                                  p_pi->compno);                   
                    truncate = 1;
-                   l_seg->newlen =p_src_data + p_max_length - l_current_data;
+                   l_seg->newlen = p_src_data + p_max_length - l_current_data;
                  
                    //opj_event_msg(p_manager, EVT_ERROR,
                    //              "read: segment too long (%d) current data (%d) p_src_data (%d) with max (%d) for codeblock %d (p=%d, b=%d, r=%d, c=%d)\n",
