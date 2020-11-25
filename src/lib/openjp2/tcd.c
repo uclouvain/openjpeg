4f487798bac3eea5ffa07d3cc542f675c4b4d9ae
yuan <zodf0055980@gmail.com>
2020-11-26 00:22:49 +0800

    Free p_tcd_marker_info to avoid memory leak

--- src/lib/openjp2/tcd.c
+++ src/lib/openjp2/tcd.c
@@ -2851,6 +2851,7 @@ void opj_tcd_marker_info_destroy(opj_tcd_marker_info_t *p_tcd_marker_info)
 {
     if (p_tcd_marker_info) {
         opj_free(p_tcd_marker_info->p_packet_size);
+        opj_free(p_tcd_marker_info);
     }
 }
 
