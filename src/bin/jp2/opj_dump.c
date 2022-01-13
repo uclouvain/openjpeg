323a08900344ddd1a2e225bd797c681e677678fd
Even Rouault <even.rouault@spatialys.com>
2022-01-13 19:22:22 +0100

    Update src/bin/jp2/opj_dump.c

--- src/bin/jp2/opj_dump.c
+++ src/bin/jp2/opj_dump.c
@@ -530,7 +530,7 @@ int main(int argc, char *argv[])
 
         for (it_image = 0; it_image < num_images; it_image++) {
             dirptr->filename[it_image] = dirptr->filename_buf + (size_t)it_image * 
-                                          OPJ_PATH_LEN;
+                                         OPJ_PATH_LEN;
         }
 
         if (load_images(dirptr, img_fol.imgdirpath) == 1) {
