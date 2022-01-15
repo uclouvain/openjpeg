43bf2bec84773ab513abb0c77d1934a2bbcf43f1
Eharve14 <71228603+Eharve14@users.noreply.github.com>
2022-01-15 09:14:13 -0500

    Re-Revised to use break statement instead of return.

--- src/bin/jp2/opj_compress.c
+++ src/bin/jp2/opj_compress.c
@@ -488,7 +488,8 @@ static unsigned int get_num_images(char *imgdirpath)
         }
         if (num_images == UINT_MAX) {
             fprintf(stderr, "Too many files in folder %s\n", imgdirpath);
-            return 0;
+            num_images = 0;
+            break;
         }
         num_images++;
     }
